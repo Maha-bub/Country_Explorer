847d7f5dd8d7238c38ba7d35cf4ff5df222170f3
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-03 09:41:12 +0600

    create a promise to load country data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import './App.css'
+import type CountryType from './type';
 
+
+const countriesPromise = async (): Promise<CountryType[]> => {
+
+  const res = await fetch('https://openapi.programming-hero.com/api/all');
+  const data = await res.json();
+  return data.countries;
+
+}
 function App() {
+
 
   return (
     <>
---
+++ src/type.ts
@@ -0,0 +1,6 @@
+export default interface CountryType {
+    name: {
+        common: string,
+        official: string
+    }
+}
